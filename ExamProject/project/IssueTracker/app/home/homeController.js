5738aeccc3bc5878fcf8cb478e6c996114bb8474
monty-georgiev <monty.georgiev@gmail.com>
2016-04-07 18:05:07 +0300

    Initial commit
    Initial commit;

---
+++ ExamProject/project/IssueTracker/app/home/homeController.js
@@ -0,0 +1,13 @@
+"use strict";
+
+angular.module('montyIssueTracker.home', [])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'app/home/homeView.html',
+            controller: 'HomeController'
+        });
+    }])
+
+    .controller('HomeController', ['$scope', function ($scope) {
+        $scope.msg = "KOR";
+    }]);
