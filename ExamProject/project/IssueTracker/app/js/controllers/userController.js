3c801340c7574e5676f4e30e54001fe0bbdc9895
monty-georgiev <monty.georgiev@gmail.com>
2016-04-11 15:52:58 +0300

    Login/Register
    Added required fields and attached them to models; Passing values to
    controller; TODO: create service;

--- ExamProject/project/IssueTracker/app/js/controllers/userController.js
+++ ExamProject/project/IssueTracker/app/js/controllers/userController.js
@@ -3,4 +3,12 @@
 angular.module('montyIssueTracker.user', [])
     .controller('UserController', ['$scope', function ($scope) {
         $scope.logged = false;
+
+        $scope.login = function (user) {
+            console.log("Login " + JSON.stringify(user));
+        };
+
+        $scope.register = function (user) {
+            console.log("Register " + JSON.stringify(user));
+        };
     }]);
